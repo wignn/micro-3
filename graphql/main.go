0d9a6b537d6d113715fb25288f8d9b58e61704c4
wignn <tigfiragnafatur1933@gmail.com>
2025-08-22 08:12:50 +0700

    refactor: Update database service dependencies and add CORS support in GraphQL server

--- graphql/main.go
+++ graphql/main.go
@@ -1,45 +1,55 @@
 package main
 
 import (
-	"log"
-	"net/http"
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/kelseyhightower/envconfig"
+    "log"
+    "net/http"
+    "github.com/99designs/gqlgen/graphql/handler"
+    "github.com/99designs/gqlgen/graphql/playground"
+    "github.com/kelseyhightower/envconfig"
+    "github.com/gorilla/handlers"
 )
 
 type AppConfig struct {
-	AccountURL string `envconfig:"ACCOUNT_SERVICE_URL"`
-	CatalogURL string `envconfig:"CATALOG_SERVICE_URL"`
-	OrderURL   string `envconfig:"ORDER_SERVICE_URL"`
-	ReviewURL  string `envconfig:"REVIEW_SERVICE_URL"`
-	AuthURL    string `envconfig:"AUTH_SERVICE_URL"`
-	CartURL    string `envconfig:"CART_SERVICE_URL"`
+    AccountURL string `envconfig:"ACCOUNT_SERVICE_URL"`
+    CatalogURL string `envconfig:"CATALOG_SERVICE_URL"`
+    OrderURL   string `envconfig:"ORDER_SERVICE_URL"`
+    ReviewURL  string `envconfig:"REVIEW_SERVICE_URL"`
+    AuthURL    string `envconfig:"AUTH_SERVICE_URL"`
+    CartURL    string `envconfig:"CART_SERVICE_URL"`
 }
 
 func main() {
-	var cfg AppConfig
-	
-	err := envconfig.Process("", &cfg)
+    var cfg AppConfig
+    
+    err := envconfig.Process("", &cfg)
 
-	if err != nil {
-		log.Fatalf("failed to process env config: %v", err)
-	}
+    if err != nil {
+        log.Fatalf("failed to process env config: %v", err)
+    }
 
-	s, err := NewGraphQLServer(cfg.AccountURL, cfg.CatalogURL, cfg.OrderURL, cfg.ReviewURL, cfg.AuthURL, cfg.CartURL)
+    s, err := NewGraphQLServer(cfg.AccountURL, cfg.CatalogURL, cfg.OrderURL, cfg.ReviewURL, cfg.AuthURL, cfg.CartURL)
 
-	if err != nil {
-		log.Fatalf("failed to create GraphQL server: %v", err)
-	}
+    if err != nil {
+        log.Fatalf("failed to create GraphQL server: %v", err)
+    }
 
-	schema, err := s.ToExecutableSchema()
-	
-	if err != nil {
-		log.Fatalf("failed to create schema: %v", err)
-	}
+    schema, err := s.ToExecutableSchema()
+    
+    if err != nil {
+        log.Fatalf("failed to create schema: %v", err)
+    }
 
-	http.Handle("/graphql", handler.NewDefaultServer(schema))
-	http.Handle("/playground", playground.Handler("GraphQL playground", "/graphql"))
+    // Setup CORS
+    corsHandler := handlers.CORS(
+        handlers.AllowedOrigins([]string{"*"}), 
+        handlers.AllowedMethods([]string{"GET", "POST", "OPTIONS"}),
+        handlers.AllowedHeaders([]string{"*"}),
+        handlers.AllowCredentials(),
+    )
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
+    mux := http.NewServeMux()
+    mux.Handle("/graphql", handler.NewDefaultServer(schema))
+    mux.Handle("/playground", playground.Handler("GraphQL playground", "/graphql"))
+
+    log.Fatal(http.ListenAndServe(":8080", corsHandler(mux)))
 }
